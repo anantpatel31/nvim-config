d7d7b8b6006263be8a60fe7aaeb7bc85110ca332
Anant Patel <anant.patel@globallogic.com>
2023-11-28 00:40:26 +0530

    28nov

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
 
+vim.opt.foldenable = false
+
 for _, source in ipairs {
   "astronvim.bootstrap",
   "astronvim.options",
